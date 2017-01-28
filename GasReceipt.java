7ce9dc8ed81292f9aebf189161e4b90add65cd13
Dennis Carrasquillo <denniscarrasquillo@Denniss-MacBook-Pro.local>
2017-01-27 21:59:43 -0700

    add exc 15

---
+++ GasReceipt.java
@@ -0,0 +1,16 @@
+public class GasReceipt{
+	public static void main(String[]args){
+		System.out.println("+----------------------------------+");
+		System.out.println("|                                  |");
+		System.out.println("|           Corner Store           |");
+		System.out.println("|                                  |");
+		System.out.println("|  2017-01-05         11:56PM      |");
+		System.out.println("|                                  |");
+		System.out.println("|  Gallons:           10.870       |");
+		System.out.println("|  Price/gallon:     $02.190       |");
+		System.out.println("|                                  |");
+		System.out.println("|  Fuel Total:       $23.805       |");
+		System.out.println("|                                  |");
+		System.out.println("+----------------------------------+");
+		}
+		}
