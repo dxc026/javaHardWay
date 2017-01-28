d2975a4c264e8f257e4c8e4f058af6dc32475b91
Dennis Carrasquillo <denniscarrasquillo@Denniss-MacBook-Pro.local>
2017-01-27 21:55:13 -0700

    add exc 10

---
+++ CreatingVariables.java
@@ -0,0 +1,32 @@
+public class CreatingVariables{
+	public static void main(String[]args){
+		int x, y, age;
+		double seconds, e, checking;
+		String firstName, lastName, title;
+		
+		x = 10;
+		y = 400;
+		age = 26;
+		
+		seconds = 4.71;
+		e = 2.71828182845904523536;
+		checking = 1.89;
+		
+		firstName = "Dennis";
+		lastName = " Carrasquillo";
+		title = "Mr. ";
+		
+		System.out.println("The variable x contains " + x);
+		System.out.println("The value " + y + " is stored in the variable y.");
+		System.out.println("The experiment took "+ seconds + " seconds.");
+		System.out.println("A favorite irrational number is Euler's number: " + e);
+		System.out.println("Hopefully you have more than $" + checking + "!");
+		System.out.println("My name's " + title + firstName + lastName);
+		
+		int yard = 40;
+		double time = 4.2;
+		String player = "Odell Beckham";
+		
+		System.out.println(player + " can run a " + time + " in the " + yard + " yard dash.");
+				}
+		}
