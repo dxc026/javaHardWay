ee0fdb8ea73802f078abc4fee0070eafec92da44
Dennis Carrasquillo <denniscarrasquillo@Denniss-MacBook-Pro.local>
2017-01-27 21:36:29 -0700

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,7 @@
+public class FirstProg{
+	public static void main(String[]args){
+		System.out.println("I, Dennis, am determined to learn how to code.");
+		System.out.println("Today's date is January 5, 2017.");
+		
+		}
+		}
